80edc6f9b58a1a1724215350a6ccfa6ccc0c8f15
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-09-03 16:51:18 +0530

    .

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then( ()=>
     console.log("mongodb connected")
 )
 
+app.set("view engine", "ejs");
 app.use(express.json());
 
 app.use("/url", urlRoute)
@@ -22,5 +23,22 @@ app.get("/:shortId", async (req, res) => {
         res.redirect(urlDoc.redirectURL)
     }
 });
+
+app.get("/", async (req, res) => {
+    const allUrls =  await URL.find({});
+     return res.end(`
+        <html>
+        <head></head>
+        <body>
+        <ol>
+            ${allUrls.map(url => `<li>${url.shortID} Points to ${url.redirectURL}</li>`).join("")}
+        </ol>
+        </body>
+        </html>
+        `
+     )
+})
+
+app.
 
 app.listen(PORT, () => console.log(`Server is live at PORT: ${PORT}`))
